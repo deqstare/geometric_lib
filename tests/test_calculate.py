b5acd97ba7a62bbc577f1b7ca7b1cc2a555e017f
Maxim Kurbatov <125643743+deqstare@users.noreply.github.com>
2024-11-04 21:42:38 +0300

    some changes with flake8 and black

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,30 +1,38 @@
 import pytest
 from calculate import calc
 
+
 def test_calc_circle_area():
-    assert calc('circle', 'area', [1]) == pytest.approx(3.14159, rel=1e-5)
+    assert calc("circle", "area", [1]) == pytest.approx(3.14159, rel=1e-5)
+
 
 def test_calc_circle_perimeter():
-    assert calc('circle', 'perimeter', [1]) == pytest.approx(6.28318, rel=1e-5)
+    assert calc("circle", "perimeter", [1]) == pytest.approx(6.28318, rel=1e-5)
+
 
 def test_calc_square_area():
-    assert calc('square', 'area', [2]) == 4
+    assert calc("square", "area", [2]) == 4
+
 
 def test_calc_square_perimeter():
-    assert calc('square', 'perimeter', [2]) == 8
+    assert calc("square", "perimeter", [2]) == 8
+
 
 def test_invalid_figure():
     with pytest.raises(AssertionError):
-        calc('invalid', 'area', [1])
+        calc("invalid", "area", [1])
+
 
 def test_invalid_function():
     with pytest.raises(AssertionError):
-        calc('circle', 'invalid', [1])
+        calc("circle", "invalid", [1])
+
 
 def test_invalid_size_for_circle():
     with pytest.raises(TypeError):
-        calc('circle', 'area', [])
+        calc("circle", "area", [])
+
 
 def test_invalid_size_for_square():
     with pytest.raises(TypeError):
-        calc('square', 'area', [2, 2])
+        calc("square", "area", [2, 2])
--- tests/test_square.py
+++ tests/test_square.py
@@ -1,14 +1,17 @@
-import pytest
 import square
+
 
 def test_square_area():
     assert square.area(2) == 4
 
+
 def test_square_perimeter():
     assert square.perimeter(2) == 8
 
+
 def test_square_area_zero():
     assert square.area(0) == 0
+
 
 def test_square_perimeter_zero():
     assert square.perimeter(0) == 0
